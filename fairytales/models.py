e71271673f906012a6d2259b1364c35ffb199c78
Marie Engel <marie.engel@deliveryhero.com>
2022-02-17 12:34:45 +0100

    SearchVectorField & GinIndex

--- fairytales/models.py
+++ fairytales/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-
+from django.contrib.postgres.indexes import GinIndex
+from django.contrib.postgres.search import SearchVectorField
 # Create your models here.
 
 
@@ -9,6 +10,10 @@ class Fairytale(models.Model):
     image = models.ImageField(null=True, blank=True, upload_to="images/")
     body = models.TextField()
     slug = models.SlugField(unique=True)
+    vector_column = SearchVectorField(null=True)  # don't use 'models.' before search
+
+    class Meta:
+        indexes = (GinIndex(fields=["vector_column"]),)
 
     def __str__(self):
-        return self.title
+        return f"{self.title} - {self.author}"
