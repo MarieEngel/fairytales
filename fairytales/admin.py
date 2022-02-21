be9a5651bd2ba4f8ba4ebe92517b93c4cb07488f
Marie Engel <marie.engel@deliveryhero.com>
2022-02-21 18:56:25 +0100

    registered profile & category

--- fairytales/admin.py
+++ fairytales/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import Fairytale
+from .models import Fairytale, Profile, Category
 
 # Register your models here.
 
 
 admin.site.register(Fairytale)
+admin.site.register(Profile)
+admin.site.register(Category)
