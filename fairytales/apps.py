9819eae8a7147e2f8258621b9bf5ebf53a6875e2
Marie Engel <marie.engel@deliveryhero.com>
2022-02-21 18:57:19 +0100

    updated fairytaleconfig

--- fairytales/apps.py
+++ fairytales/apps.py
@@ -1,6 +1,10 @@
 from django.apps import AppConfig
 
 
+
 class FairytalesConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "fairytales"
+
+    # def ready(self):
+    #     import users.signals
