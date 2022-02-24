2884490b9c103b90b85cd9477e88a506590ea34e
Marie Engel <marie.engel@deliveryhero.com>
2022-02-24 13:03:31 +0100

    and logout

--- fairytales/tests/test_login.py
+++ fairytales/tests/test_login.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
 
-class LoginTest(TestCase):
+class LoginLogoutTest(TestCase):
 
     def setUp(self):
             self.user = User.objects.create_user(
@@ -19,4 +19,13 @@ class LoginTest(TestCase):
         response = self.client.get('/fairytales/')
         self.assertEquals(response.status_code, 200)
         self.assertContains(response, 'Logout')
-        self.assertTrue(self.user.is_active)
+    
+
+    def test_logout(self):
+        response = self.client.get('/fairytales/logout/')
+        self.assertEquals(response.status_code, 302)
+
+        self.client.logout()
+        response = self.client.get('/fairytales/')
+        self.assertContains(response, 'Login')
+       
